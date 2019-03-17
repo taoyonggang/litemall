f90d78140e8077e58c3cbdc027f1a2427cf3b65d
taoyg <11379811@qq.com>
2019-03-17 23:22:50 +0800

    add utils.js

---
+++ litemall-wx/utils/utils.js
@@ -0,0 +1,15 @@
+let getQueryString = function (url, name) {
+  console.log("url = " + url)
+  console.log("name = " + name)
+  var reg = new RegExp('(^|&|/?)' + name + '=([^&|/?]*)(&|/?|$)', 'i')
+  var r = url.substr(1).match(reg)
+  if (r != null) {
+    console.log("r = " + r)
+    console.log("r[2] = " + r[2])
+    return r[2]
+  }
+  return null;
+}
+module.exports = {
+  getQueryString: getQueryString,
+}
