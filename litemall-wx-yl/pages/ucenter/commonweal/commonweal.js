fe5e0ff2d247b6cd0683dc688215e519d55ead6b
wangling <1786714994@qq.com>
2019-04-08 10:24:57 +0800

    修改悠蓝及海普小程序的样式

--- litemall-wx-yl/pages/ucenter/commonweal/commonweal.js
+++ litemall-wx-yl/pages/ucenter/commonweal/commonweal.js
@@ -14,10 +14,10 @@ Page({
     showPage: false,
     hasMoreData: true,
     integralSum: 0,
-    integral:0,
+    integral: 0,
 
   },
-  onLoad: function(options) {
+  onLoad: function (options) {
     let that = this;
     util.request(api.IntegralsIndex, {
       type: that.data.type,
@@ -37,37 +37,37 @@ Page({
       integral: parseInt(e.detail.value)
     });
   },
-  saveDonate(){
+  saveDonate() {
     let that = this;
     if (parseInt(that.data.integral) < 500 || parseInt(that.data.integral) === 500) {
-        wx.showToast({
-          title: '请输入大于等于500的正整数',
-          icon: 'none',
-          duration: 2000
-        });
-        return false;
-        }else{
-          util.request(api.SaveDonate, {
-            integral: that.data.integral,
-          }).then(function (res) {
-            if (res.errno === 0) {
-              wx.showToast({
-                title: '捐赠成功',
-                icon: 'success',
-              });
-            }
-
+      wx.showToast({
+        title: '请输入大于等于500的正整数',
+        icon: 'none',
+        duration: 2000
+      });
+      return false;
+    } else {
+      util.request(api.SaveDonate, {
+        integral: that.data.integral,
+      }).then(function (res) {
+        if (res.errno === 0) {
+          wx.showToast({
+            title: '捐赠成功',
+            icon: 'success',
           });
         }
-      
-   
+
+      });
+    }
+
+
   },
-  listDnonate(){
+  listDnonate() {
     wx.navigateTo({
       url: "/pages/ucenter/commonwealrecord/commonwealrecord"
     });
   },
-  rankDonate(){
+  rankDonate() {
     wx.navigateTo({
       url: "/pages/ucenter/commonwealrank/commonwealrank"
     });
