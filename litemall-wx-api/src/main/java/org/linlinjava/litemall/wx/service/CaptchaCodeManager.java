f413090865f4d98c93ef6a14cde88d2088fa9c3f
taoyg <11379811@qq.com>
2019-04-24 22:15:58 +0800

    修改手机注册码过期时间为3分钟

--- litemall-wx-api/src/main/java/org/linlinjava/litemall/wx/service/CaptchaCodeManager.java
+++ litemall-wx-api/src/main/java/org/linlinjava/litemall/wx/service/CaptchaCodeManager.java
@@ -34,8 +34,8 @@ public class CaptchaCodeManager {
         CaptchaItem captchaItem = new CaptchaItem();
         captchaItem.setPhoneNumber(phoneNumber);
         captchaItem.setCode(code);
-        // 有效期为1分钟
-        captchaItem.setExpireTime(LocalDateTime.now().plusMinutes(1));
+        // 有效期为3分钟
+        captchaItem.setExpireTime(LocalDateTime.now().plusMinutes(3));
 
         captchaCodeCache.put(phoneNumber, captchaItem);
 
