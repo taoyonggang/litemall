764921323a7476983910acea6248fe97a8c862aa
wanglingannie <1786714994@qq.com>
2019-04-26 17:14:04 +0800

    修改后台管理会员展示及库存更新、会员活动记录展示增加字段

--- litemall-db/src/main/java/org/linlinjava/litemall/db/domain/LitemallActivityMore.java
+++ litemall-db/src/main/java/org/linlinjava/litemall/db/domain/LitemallActivityMore.java
@@ -1,5 +1,6 @@
 package org.linlinjava.litemall.db.domain;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -11,7 +12,42 @@ public class LitemallActivityMore extends LitemallActivity{
       private String username;
       private String nickname;
       private String mobile;
+      private LocalDate babybirthday;
+      private LocalDate babybirthday2;
+      private String memberUsername;
+      private String subtitle;
 
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+
+    public LocalDate getBabybirthday() {
+        return babybirthday;
+    }
+
+    public void setBabybirthday(LocalDate babybirthday) {
+        this.babybirthday = babybirthday;
+    }
+
+    public LocalDate getBabybirthday2() {
+        return babybirthday2;
+    }
+
+    public void setBabybirthday2(LocalDate babybirthday2) {
+        this.babybirthday2 = babybirthday2;
+    }
+
+    public String getMemberUsername() {
+        return memberUsername;
+    }
+
+    public void setMemberUsername(String memberUsername) {
+        this.memberUsername = memberUsername;
+    }
     public String getTitle() {
         return title;
     }
